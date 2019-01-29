7c53e5fffca3401ede8237b26e2a3faa999fecda
yankovskyandrey <YankovskyAndrey>
2019-01-29 13:47:43 +0300

    Добавил метод получения всех лидов

--- index.js
+++ index.js
@@ -1,53 +1,77 @@
 const fetch = require('isomorphic-unfetch')
+const querystring = require('querystring');
 
 module.exports = (baseUrl, userLogin, userHash) => {
+	const authQueryParams = {
+		USER_LOGIN: userLogin,
+		USER_HASH: userHash,
+	}
+
+	const getAmoApiUrl = (apiPath, query) =>
+		`${baseUrl}${apiPath}?${querystring.stringify({...authQueryParams, ...query})}`
+
+	const getAmoItems = (getRequestUrl, requestedItemsCount) => {
+		const getItemsAcc = (offset, itemsAcc) =>
+			fetch(getRequestUrl(offset), {
+				method: 'GET',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+			}).then(response => {
+				const status = response.status
+				if (status === 204) {
+					return null
+				}
+				if (status === 200) {
+					return response.json()
+				}
+				return response.text().then(error => {
+					console.error(status, error)
+					return null
+				}).catch(error => {
+					console.error(error)
+					return null
+				})
+			}).then(result => {
+				if (!result) {
+					return itemsAcc
+				}
+
+				const items = result._embedded.items
+				if (items.length < requestedItemsCount) {
+					return itemsAcc.concat(items)
+				}
+
+				return getItemsAcc(offset + requestedItemsCount, itemsAcc.concat(items))
+			})
+
+		return getItemsAcc(0, [])
+	}
+
 	return {
 		// TODO > timestamp на вход
-		// Контакты отдаются по 500, поэтому нам надо вызывать этот запрос с правильным offset
-		// пока не загрузим все контакты
+		// Контакты отдаются по 500, поэтому нам надо вызывать этот запрос с правильным offset пока не загрузим все контакты
 		getContacts: () => {
-			const AUTH_QUERY_PARAMS = `USER_LOGIN=${userLogin}&USER_HASH=${userHash}`
 			const REQUESTED_CONTACTS_COUNT = 500
 
-			const getContactsRequestUrl = offset =>
-				`${baseUrl}/api/v2/contacts/?limit_rows=${REQUESTED_CONTACTS_COUNT}&limit_offset=${offset}&${AUTH_QUERY_PARAMS}`
+			const getContactsRequestUrl = offset => getAmoApiUrl('/api/v2/contacts/', {
+				limit_rows: REQUESTED_CONTACTS_COUNT,
+				limit_offset: offset
+			})
 
-			const getContactsAcc = (offset, contacts) =>
-				fetch(getContactsRequestUrl(offset), {
-					method: 'GET',
-					headers: {
-						Accept: 'application/json',
-						'Content-Type': 'application/json',
-					},
-				}).then(response => {
-					const status = response.status
-					if (status === 204) {
-						return null
-					}
-					if (status === 200) {
-						return response.json()
-					}
-					return response.text().then(error => {
-						console.error(status, error)
-						return null
-					}).catch(error => {
-						console.error(error)
-						return null
-					})
-				}).then(result => {
-					if (!result) {
-						return contacts
-					}
+			return getAmoItems(getContactsRequestUrl, REQUESTED_CONTACTS_COUNT)
+		},
+		// Контакты отдаются по 500, поэтому нам надо вызывать этот запрос с правильным offset пока не загрузим все контакты
+		getLeads: () => {
+			const REQUESTED_LEADS_COUNT = 500
 
-					const items = result._embedded.items
-					if (items.length < REQUESTED_CONTACTS_COUNT) {
-						return contacts.concat(items)
-					}
+			const getLeadsRequestUrl = offset => getAmoApiUrl('/api/v2/leads/', {
+				limit_rows: REQUESTED_LEADS_COUNT,
+				limit_offset: offset
+			})
 
-					return getContactsAcc(offset + REQUESTED_CONTACTS_COUNT, contacts.concat(items))
-				})
-
-			return getContactsAcc(0, [])
+			return getAmoItems(getLeadsRequestUrl, REQUESTED_LEADS_COUNT)
 		},
 	}
 }
--- test.js
+++ test.js
@@ -2,4 +2,10 @@ const AmoUtils = require('./index')
 
 const { AMO_CRM_BASE_URL, AMO_CRM_USER_LOGIN, AMO_CRM_USER_HASH } = process.env
 
-AmoUtils(AMO_CRM_BASE_URL, AMO_CRM_USER_LOGIN, AMO_CRM_USER_HASH).getAllContacts().then(c => console.log(c))
+Promise.all([
+	AmoUtils(AMO_CRM_BASE_URL, AMO_CRM_USER_LOGIN, AMO_CRM_USER_HASH).getContacts(),
+	AmoUtils(AMO_CRM_BASE_URL, AMO_CRM_USER_LOGIN, AMO_CRM_USER_HASH).getLeads(),
+]).then(([contacts, leads]) => {
+	console.log('Contacts: ', contacts.length)
+	console.log('Leads: ', leads.length)
+})
